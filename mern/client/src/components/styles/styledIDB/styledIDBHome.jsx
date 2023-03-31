2f06878bb7df4d3cfd83c4c25dda1130091de449
TayisiyaChernenko <tcx190026@utdallas.edu>
2023-03-31 14:32:32 -0500

    styling reorganization of discussionBoard components

--- mern/client/src/components/styles/styledIDB/styledIDBHome.jsx
+++ mern/client/src/components/styles/styledIDB/styledIDBHome.jsx
@@ -1,26 +1,5 @@
 import styled from "styled-components";
 
-export const StyledPosts = styled.div `
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    border: 1px solid black;
-    border-radius: 25px;
-    width: 18vw;
-    height: 16vh;
-    background-color: #EEEEEE;
-    box-shadow: 2px 2px 2px 1px rgba(5, 60, 10, 0.8);
-    margin: 0 0 20px 0;
-`;
-export const StyledResponse = styled.div `
-    display: flex;
-    flex-direction: column;
-    float:right;
-    background-color: #EEEEEE;
-    width: 10vw;
-    height: 8vh; 
-    margin: 10px 0px 20px 0;
-`;
 export const StyledPrevPosts = styled.div`
     display: flex;
     flex-direction: column;
