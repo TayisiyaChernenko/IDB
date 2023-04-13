ac9fd326b68c4e0bca7f022d98b8f591095de33e
TayisiyaChernenko <tcx190026@utdallas.edu>
2023-04-13 14:54:26 -0500

    ,

--- mern/server/model.js
+++ mern/server/model.js
@@ -2,3 +2,18 @@ const mongoose = require('mongoose');
 const Db = process.env.ATLAS_URI;
 
 await mongoose.connect(Db);
+
+
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+const Post = new Schema({
+  postID: ObjectId,
+  title: String,
+  body: String,
+  date: Date
+});
+
+const User = new Schema({
+
+});
