c75b997184aaa376fc525617042eca80982fe038
kxn200007 <115383089+kxn200007@users.noreply.github.com>
2023-04-14 17:40:47 -0500

    Update server.js

--- mern/server/server.js
+++ mern/server/server.js
@@ -1,80 +1,71 @@
 const express = require('express');
-const MongoClient = require('mongodb').MongoClient;
-const ObjectId = require('mongodb').ObjectId;
-const jwt = require('jsonwebtoken');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 require("dotenv").config({ path: "./config.env" });
 const PORT = process.env.PORT || 3000;
 const Db = process.env.ATLAS_URI;
-var mongoose = require('mongoose');
-var Posts = require('./models/postModel');
-var Users = require('./models/userModel');
+const mongoose = require('mongoose');
+const Posts = require('./models/postModel');
+const Users = require('./models/userModel');
 
-
-// Create an Express app instance
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+mongoose.connect(Db, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 
-// Connect to the MongoDB database
-//const uri = 'mongodb://localhost:27017/';
-const client = new MongoClient(Db, { useNewUrlParser: true, useUnifiedTopology: true });
-
-
-client.connect((err) => {
-  if (err) throw err;
-  const dbase = client.db('discussion_board');
-});
-
-mongoose.connect(Db);
-
-// Secret key for JWT
-const SECRET_KEY = 'mysecretkey';
-/*
-// API endpoint for user authentication
-app.post('/auth/login', async (req, res) => {
-  const { username, password } = req.body;
-  try {
-    const user = await usersCollection.findOne({ username, password });
-    if (user) {
-      const token = jwt.sign({ userId: user._id }, SECRET_KEY);
-      res.json({ token });
-    } else {
-      res.status(401).json({ message: 'Invalid username or password' });
-    }
-  } catch (error) {
-    res.status(500).json({ message: 'Error during authentication' });
-  }
-});*/
-
-//// REGISTRATION ////
-
-// API endpoint for a new user 
 app.post('/discussions/register', async (req, res) => {
-  var newUser = new Users ({
+  var newUser = new Users({
     email: req.body.email,
     firstName: req.body.firstName,
     lastName: req.body.lastName,
     password: req.body.password,
-    });
-  const createdUser = await newUser.save();
-  return res.status(201).json(createdUser);
+  });
+  try {
+    const createdUser = await newUser.save();
+    return res.status(201).json(createdUser);
+  } catch (error) {
+    res.status(500).json({ message: 'Error during registration' });
+  }
 });
 
-//API endpoint to get user
-
-// API endpoint for fetching all posts
 app.get('/api/posts', async (req, res) => {
   try {
-    const posts = await Posts.find({}).toArray();
+    const posts = await Posts.find({});
     res.json(posts);
   } catch (error) {
     res.status(500).json({ message: 'Error fetching posts' });
   }
 });
- /**
+
+app.post('/discussions', async (req, res) => {
+  var newPost = new Posts({
+    text: req.body.text,
+    courseName: req.body.courseName,
+    sectionNum: req.body.sectionNum,
+    datePosted: req.body.datePosted,
+    replies: req.body.replies || [],
+  });
+  try {
+    const createdPost = await newPost.save();
+    res.status(201).json(createdPost);
+  } catch (error) {
+    res.status(500).json({ message: 'Error creating discussion' });
+  }
+});
+
+app.delete('/api/posts/:id', async (req, res) => {
+  const postId = req.params.id;
+  try {
+    await Posts.findByIdAndDelete(postId);
+    res.json({ message: 'Post deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ message: 'Error deleting post' });
+  }
+});
+
+/*
+// Other API endpoints and functions are commented out here
 // API endpoint for deleting a specific post
 app.delete('/api/posts/:id', async (req, res) => {
   const postId = req.params.id;
@@ -85,7 +76,6 @@ app.delete('/api/posts/:id', async (req, res) => {
     res.status(500).json({ message: 'Error deleting post' });
   }
 });
-
 // API endpoint for fetching all discussions
 app.get('/discussions', async (req, res) => {
   try {
@@ -95,7 +85,6 @@ app.get('/discussions', async (req, res) => {
     res.status(500).json({ message: 'Error fetching discussions' });
   }
 });
-
 // API endpoint for fetching a specific discussion
 app.get('/discussions/:id', async (req, res) => {
   const discussionId = req.params.id;
@@ -106,7 +95,6 @@ app.get('/discussions/:id', async (req, res) => {
     res.status(500).json({ message: 'Error fetching discussion' });
   }
 });
-
 // API endpoint for creating a new discussion
 app.post('/discussions', async (req, res) => {
   const discussion = req.body;
@@ -117,7 +105,6 @@ app.post('/discussions', async (req, res) => {
     res.status(500).json({ message: 'Error creating discussion' });
   }
 });
-
 // API endpoint for adding a comment to a specific discussion
 app.post('/discussions/:id/comments', async (req, res) => {
   const discussionId = req.params.id;
@@ -131,7 +118,6 @@ app.post('/discussions/:id/comments', async (req, res) => {
     res.status(500).json({ message: 'Error adding comment' });
   }
 });
-
 // API endpoint for deleting a specific discussion
 app.delete('/discussions/:id', async (req, res) => {
   const discussionId = req.params.id;
@@ -142,7 +128,6 @@ app.delete('/discussions/:id', async (req, res) => {
     res.status(500).json({ message: 'Error deleting discussion' });
   }
 });
-
 // API endpoint for updating a specific discussion
 app.put('/discussions/:id', async (req, res) => {
   const discussionId = req.params.id;
@@ -157,7 +142,6 @@ app.put('/discussions/:id', async (req, res) => {
     res.status(500).json({ message: 'Error updating discussion' });
   }
 });
-
 // API endpoint for admin to delete a specific discussion
 app.delete('/admin/discussions/:id', async (req, res) => {
   const discussionId = req.params.id;
@@ -172,7 +156,6 @@ app.delete('/admin/discussions/:id', async (req, res) => {
     res.status(403).json({ message: 'Not authorized' });
   }
 });
-
 // Function to check if a user is an admin
 async function isAdmin(request) {
   const token = request.headers.authorization;
@@ -180,11 +163,9 @@ async function isAdmin(request) {
   if (!token) {
     return false;
   }
-
   try {
     const decoded = jwt.verify(token, SECRET_KEY);
     const user = await usersCollection.findOne({ _id: ObjectId(decoded.userId) });
-
     return user && user.role === 'admin';
   } catch (error) {
     return false;
@@ -192,8 +173,6 @@ async function isAdmin(request) {
 }
 */
 
-
 app.listen(PORT, () => {
-
   console.log(`Server is running on port ${PORT}`);
 });
